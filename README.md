42ebd2545ddc8a631573118f4dbfe08394d09c13
harperfu6 <beharp8a8@gmail.com>
2023-05-31 17:08:43 +0900

    add api path

--- README.md
+++ README.md
@@ -8,20 +8,26 @@ $ cargo r
 # request by client
 ## POST data
 ```
-$ curl -X POST -H "Content-Type: application/json" -d '{"key": "value"}' localhost:8080
+$ curl -X POST -H "Content-Type: application/json" -d '{"key": "value"}' localhost:8080/api/kv
 ```
 ## GET data
 ```
-$ curl localhost:8080/{key}
+$ curl localhost:8080/api/kv/{key}
+```
+
+## health check
+```
+$ curl localhost:8080/health
+OK
 ```
 
 ## example
 ```
 # POST DATA
-$ curl -X POST -H "Content-Type: application/json" -d '{"name":"daiki", "age":"30"}' localhost:8080
+$ curl -X POST -H "Content-Type: application/json" -d '{"name":"daiki", "age":"30"}' localhost:8080/api/kv
 The specified key was successfully created.
 
 # GET by key
-$ curl localhost:8080/name
+$ curl localhost:8080/api/kv/name
 daiki
 ```
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,8 @@
 use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 
 use log::info;
-use warp::Filter;
+use reqwest::StatusCode;
+use warp::{path, reply::WithStatus, Filter};
 
 use crate::kvstore::KvStore;
 
@@ -16,30 +17,38 @@ impl Server {
         let bind_endpoint = SocketAddr::from(([127, 0, 0, 1], 8080));
         info!("Listening on {}", bind_endpoint);
 
-        let store = warp::any().map(move || store.clone());
-
-        let prompt = warp::get()
-            .and(warp::path("hello"))
-            .and(warp::path::param())
-            .map(|name: String| format!("Hello, {}!", name))
-            .or(warp::post()
-                .and(store.clone())
-                .and(warp::body::json::<HashMap<String, String>>())
-                .and_then(post_data))
-            .or(warp::get()
-                .and(store.clone())
-                .and(warp::path::param())
-                .and_then(get_data));
-
-        warp::serve(prompt).run(bind_endpoint).await;
+        warp::serve(route_filter(store)).run(bind_endpoint).await;
     }
+}
+
+fn route_filter(
+    store: Arc<KvStore>,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    let store = warp::any().map(move || store.clone());
+
+    let api_kv = warp::get()
+        .and(store.clone())
+        .and(path!("api" / "kv" / String))
+        .and_then(get_data)
+        .or(warp::post()
+            .and(store.clone())
+            .and(path!("api" / "kv"))
+            .and(warp::body::json::<HashMap<String, String>>())
+            .and_then(post_data));
+
+    let health = warp::path("health").map(|| Ok(warp::reply::with_status("OK", StatusCode::OK)));
+
+    api_kv.or(health)
 }
 
 async fn get_data(store: Arc<KvStore>, key: String) -> Result<impl warp::Reply, warp::Rejection> {
     info!("GET by: {}", key);
     match store.get(key) {
-        Some(v) => Ok::<_, warp::Rejection>(v),
-        None => Ok::<_, warp::Rejection>("The specified key does not exist.".to_string()),
+        Some(v) => Ok(warp::reply::with_status(v, StatusCode::OK)),
+        None => Ok(warp::reply::with_status(
+            "The specified key does not exist.".to_string(),
+            StatusCode::NOT_FOUND,
+        )),
     }
 }
 
@@ -49,7 +58,10 @@ async fn post_data(
 ) -> Result<impl warp::Reply, warp::Rejection> {
     info!("POST DATA: {:?}", data);
     store.set(data);
-    Ok::<_, warp::Rejection>("The specified key was successfully created.")
+    Ok(warp::reply::with_status(
+        "The specified key was successfully created.".to_string(),
+        StatusCode::CREATED,
+    ))
 }
 
 #[cfg(test)]
@@ -59,7 +71,7 @@ mod tests {
     #[tokio::test]
     async fn server_test_all() {
         server_run().await;
-        hello_world().await;
+        health().await;
         put_and_get_data().await;
     }
 
@@ -71,28 +83,28 @@ mod tests {
         });
     }
 
-    async fn hello_world() {
-        let response = reqwest::get("http://localhost:8080/hello/world")
+    async fn health() {
+        let response = reqwest::get("http://localhost:8080/health")
             .await
             .unwrap()
             .text()
             .await
             .unwrap();
 
-        assert_eq!(response, "Hello, world!");
+        assert_eq!(response, "OK");
     }
 
     async fn put_and_get_data() {
         let client = reqwest::Client::new();
         let _ = client
-            .post("http://localhost:8080")
+            .post("http://localhost:8080/api/kv")
             .json(&HashMap::from([("key".to_string(), "value".to_string())]))
             .send()
             .await
             .unwrap();
 
         let response = client
-            .get("http://localhost:8080/key")
+            .get("http://localhost:8080/api/kv/key")
             .send()
             .await
             .unwrap()
